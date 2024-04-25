8e4e35ef0d283d2444c1c1f2b8c2f8b615fc399d
miniguiti <giuliana.franca@sptech.school>
2024-04-25 14:11:50 -0300

    Formatação da data

--- js/buscar.js
+++ js/buscar.js
@@ -1,39 +1,36 @@
-async function buscar(){
+async function buscar() {
     const resposta = await fetch("https://660f44b0356b87a55c510db2.mockapi.io/agendas");
     const respostaDadosAgenda = await resposta.json();
 
     console.log("Resposta: ", respostaDadosAgenda);
 
-   const cards = document.getElementById("cards_games");
-   
+    const cards = document.getElementById("cards_games");
+
     cards.innerHTML = respostaDadosAgenda.map((itemAgenda) => {
-           return `
-           <div class="cardItem">
-           <div class="dataGame"> 
-               <img src="../imagens/calendar-solid.svg" alt=""> ${itemAgenda.dataJogo} 
-           </div>
-           <img class="img-background" src="${itemAgenda.urlImagem}" alt="">
-           <div class="descricao">
-               <p> <strong>${itemAgenda.nome}</strong> </p>
-               <p> ${itemAgenda.descricao} </p>
-               <p><strong>Gamers: </strong></p>
-
-               <div class="gamers">
-               ${
-                itemAgenda.gamers.map((gamerItem) => {
-                    return `<div class="gamerItem">@${gamerItem}</div>`
-                }).join('')
-               }
-                
-               </div>
-
-             
-           </div>  
-           <div class="assistir">
-                   <a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
-           </div>
-       </div>
-           ` 
+        var dataAgenda = new Date(itemAgenda.dataJogo);
+            return `
+            <div class="cardItem">
+                <div class="dataGame"> 
+                    <img src="../imagens/calendar-solid.svg" alt=""> 
+                    ${dataAgenda.getDay()}/${dataAgenda.getMonth()}/${dataAgenda.getFullYear()} 
+                </div>
+                <img class="img-background" src="${itemAgenda.urlImagem}" alt="">
+                <div class="descricao">
+                    <p> <strong>${itemAgenda.nome} </strong></p>
+                    <p> ${itemAgenda.descricao} </p>
+                    <p><strong>Gamers: </strong></p>
+                    <div class="gamers">
+                    ${itemAgenda.gamers.map((gamerItem) => {
+                        return `<div class="gamerItem">@${gamerItem}</div>`
+                        }).join('')
+                    }
+                </div>
+                </div>  
+                <div class="assistir">
+                    <a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+                </div>
+            </div>
+           `
     }).join((''));
 }
 
